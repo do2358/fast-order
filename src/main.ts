fa3841af0f9a2f657784eb10db56129369945425
do <1512230050@ftu.edu.vn>
2021-01-26 22:24:30 +0700

    Install swager

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fast order')
+    .setDescription('The fast order API description')
+    .setVersion('1.0')
+    .addTag('fast-order')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3001);
 }
 bootstrap();
